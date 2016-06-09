e52f9f5d2b3d2d838f20c1f56e84db33593729e7
nim- <nime1312@gmail.com>
2016-06-09 15:52:35 +0200

    More pseudocode

--- a.c
+++ a.c
@@ -1,33 +1,110 @@
 #include <stdio>;
 
-int int main(int argc, char const *argv[]) {
+struct vector{
+  int x;
+  int y;
+};
+
+int cells[xMAX][yMAX][2]; //x, y, life/type
+
+struct vector v;
+
+int main(int argc, char const *argv[]) {
   /* code */
   return 0;
 }
 
-int angleCalc(int s, int v_v1, intv_v2){
-    int vinkel, v_s1, v_s2;
+int angleCalc(int s, int v.x, int v.y){
+    int vinkel, v.x, v.y;
 
     if(s==0){
-      v_s1=0;,
-      v_s2=1;
+      v.x=0;,
+      v.y=1;
     }else if(s==1){
-      v_s1=1;
-      v_s2=0;
+      v.x=1;
+      v.y=0;
     }else {
       //error
     }
 
-    vinkel = (180-arccos((v_s1*v_v1+v_s2*v_v2)/sqrt(pow(v_v1,2)+pow(v_v2,2))))/2;//udgangsvinklen af bolden
+    vinkel = (180-arccos((v.x*v.x+v.y*v.y)/sqrt(pow(v.x,2)+pow(v.x,2))))/2;//udgangsvinklen af bolden
 
     return vinkel;
 }
 
-int new_vector(){//ny vektor efter den har ramt en væg eller blok
+int newVector(){//ny vektor efter den har ramt en væg eller blok
   int a, b;
 
-  a=sqrt(pow(v_v1,2)+pow(v_v2,2))*cos(vinkel);
-  b=sqrt(pow(v_v1,2)+pow(v_v2,2))*sin(vinkel);
+  a=sqrt(pow(v.x,2)+pow(v.y,2))*cos(vinkel);
+  b=sqrt(pow(v.x,2)+pow(v.y,2))*sin(vinkel);
 
   return a,b;
+}
+
+int edge(){
+  int s;
+  switch (v.x){
+      case 0:
+        s = 0;
+        break;
+      case X_MAX:
+        s = 0;
+        break;
+      case default:
+        //error
+        break;
+  }
+
+  switch (v.y){
+      case 0:
+        killPlayer();
+        break;
+      case X_MAX:
+        s = 0;
+        break;
+      case default:
+        //error
+        break;
+  }
+  return s;
+}
+
+void releaseBall(){
+  while (1){
+    if (PLAYER_INPUT_RELEASE_BUTTON == 1){
+      v.x = 1;
+      v.y = 1;
+      break;
+    }
+  }
+}
+
+void vectorNextCell(){
+  /* typer:
+  0 = ingenting
+  1 = wall
+  2 = brick
+  3 = striker  */
+  (int newX, int newY) = newVector();
+
+  if (cells[newX][newY][1]==0){
+    v.x = newX;
+    v.y = newY;
+  } else if (cells[newX][newY][1]==1){
+    //angleCalc
+  }else if (cells[newX][newY][1]==2){
+    /*cells[newX][newY][0]--;
+    alive??
+    angleCalc
+    */
+  } else if (cells[newX][newY][1]==3){
+    strikeCalc();
+  } else {
+    killPlayer();
+  }
+
+}
+
+void killPlayer(){
+
 }
